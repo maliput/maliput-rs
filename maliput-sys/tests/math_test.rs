a6facbe71b28a3aac4896348bd0cb3efaaf731ca
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2024-02-09 17:58:12 -0300

    Adds bindings to maliput::math::Vector3. (#12)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- maliput-sys/tests/math_test.rs
+++ maliput-sys/tests/math_test.rs
@@ -28,6 +28,51 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-pub mod api;
-pub mod math;
-pub mod plugin;
+#[cfg(test)]
+mod math_test {
+    use maliput_sys::math::ffi::Vector3_new;
+
+    use maliput_sys::math::ffi::Vector3_cross;
+    use maliput_sys::math::ffi::Vector3_dot;
+    use maliput_sys::math::ffi::Vector3_norm;
+    use maliput_sys::math::ffi::Vector3_equals;
+
+    #[test]
+    fn vector3_new() {
+        let v = Vector3_new(1.0, 2.0, 3.0);
+        assert_eq!(v.x(), 1.0);
+        assert_eq!(v.y(), 2.0);
+        assert_eq!(v.z(), 3.0);
+    }
+
+    #[test]
+    fn vector3_norm() {
+        let v = Vector3_new(1.0, 2.0, 3.0);
+        assert_eq!(Vector3_norm(&v), (v.x()*v.x() + v.y()*v.y() + v.z()*v.z()).sqrt());
+    }
+
+    #[test]
+    fn vector3_dot() {
+        let v = Vector3_new(1.0, 2.0, 3.0);
+        let w = Vector3_new(4.0, 5.0, 6.0);
+        assert_eq!(Vector3_dot(&v, &w), v.x()*w.x() + v.y()*w.y() + v.z()*w.z());
+    }
+
+    #[test]
+    fn vector3_cross() {
+        let v = Vector3_new(1.0, 2.0, 3.0);
+        let w = Vector3_new(4.0, 5.0, 6.0);
+        let cross = Vector3_cross(&v, &w);
+        assert_eq!(cross.x(), v.y()*w.z() - v.z()*w.y());
+        assert_eq!(cross.y(), v.z()*w.x() - v.x()*w.z());
+        assert_eq!(cross.z(), v.x()*w.y() - v.y()*w.x());
+    }
+
+    #[test]
+    fn vector3_equals() {
+        let v = Vector3_new(1.0, 2.0, 3.0);
+        let w = Vector3_new(1.0, 2.0, 3.0);
+        assert!(Vector3_equals(&v, &w));
+    }
+
+}
