0703c081c2e9a8b82945c9c352509d61e4574790
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2025-11-05 16:42:31 -0300

    Add `RuleRegistry.get_possible_states_of_rule_type` API. (#243)
    * Document and rustify code.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    * Add tests.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    * Improve naming.
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>

--- maliput/tests/rule_registry_test.rs
+++ maliput/tests/rule_registry_test.rs
@@ -28,7 +28,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use maliput::api::rules::RuleType;
+use maliput::api::rules::{RuleType, RuleValuesByType};
 
 mod common;
 
@@ -62,4 +62,17 @@ fn rule_registry_test() {
     assert!(range_values.is_some());
     let range_values = range_values.unwrap();
     assert!(!range_values.is_empty());
+
+    let rule_value_types = rule_registry.get_possible_states_of_rule_type(RuleType::RightOfWay.to_string());
+    assert!(rule_value_types.is_some());
+    let rule_value_types = rule_value_types.unwrap();
+    assert!(matches!(rule_value_types, RuleValuesByType::DiscreteValues(_)));
+    let rule_value_types = rule_registry.get_possible_states_of_rule_type(RuleType::SpeedLimit.to_string());
+    assert!(rule_value_types.is_some());
+    let rule_value_types = rule_value_types.unwrap();
+    assert!(matches!(rule_value_types, RuleValuesByType::Ranges(_)));
+
+    assert!(rule_registry
+        .get_possible_states_of_rule_type("InvalidRuleType".to_string())
+        .is_none());
 }
