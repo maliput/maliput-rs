b7a0686073dd98e5c536863d963f87633e00b030
Santiago Lopez <santiago.lopez@ekumenlabs.com>
2025-12-09 18:29:10 -0300

    Update copywrite comment.
    Signed-off-by: Santiago Lopez <santiago.lopez@ekumenlabs.com>

--- maliput/tests/intersection_book_test.rs
+++ maliput/tests/intersection_book_test.rs
@@ -1,6 +1,6 @@
 // BSD 3-Clause License
 //
-// Copyright (c) 2024, Woven by Toyota.
+// Copyright (c) 2025, Woven by Toyota.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
